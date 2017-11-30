34dbea9c9d4440eed33f08aa2c385806b8fc14e1
nga huynh <nga181096@gmail.com>
2017-11-30 11:32:49 +0700

    update

--- app/src/main/java/com/example/mattershmily/myapplication/caidat.java
+++ app/src/main/java/com/example/mattershmily/myapplication/caidat.java
@@ -29,8 +29,8 @@ public class caidat extends AppCompatActivity {
         Database=database.initDatabase(this,DATABASE_NAME);
         Cursor cursor=Database.rawQuery("SELECT * FROM caidat",null);
         cursor.moveToFirst();
-        final String s=cursor.getString(1);
-        final String s2=cursor.getString(2);
+        final String ddkn=cursor.getString(1);
+        final String ddck=cursor.getString(2);
         //Khởi tạo toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,23 +43,36 @@ public class caidat extends AppCompatActivity {
             lvDb=(ListView) findViewById(R.id.lv);
             arrayDb=new ArrayList<>();
             //gan du lieu cho mang trong listview
-            arrayDb.add(new item_caidat("Độ dài kinh nguyệt","",R.drawable.icon_hoa));
-            arrayDb.add(new item_caidat("Độ dài chu kỳ","",R.drawable.clock));
-            arrayDb.add(new item_caidat("Giai đoạn hoàng thể","",R.drawable.book));
-            arrayDb.add(new item_caidat("Nhắc nhở","",R.drawable.nhacnho));
-            arrayDb.add(new item_caidat("Triệu chứng và tâm trạng","",R.drawable.book));
-            arrayDb.add(new item_caidat("Mật khẩu","",R.drawable.home));
-            arrayDb.add(new item_caidat("Ngôn ngữ","",R.drawable.caidat));
+            arrayDb.add(new item_caidat("Độ dài kinh nguyệt","ddkn",R.drawable.icon_setting_period));
+            arrayDb.add(new item_caidat("Độ dài chu kỳ","ddck",R.drawable.icon_setting_period));
+            arrayDb.add(new item_caidat("Rụng trứng","",R.drawable.icon_setting_ovulation));
+            arrayDb.add(new item_caidat("Nhắc nhở","",R.drawable.icon_setting_nhacnho));
+            arrayDb.add(new item_caidat("Triệu chứng và tâm trạng","",R.drawable.icon_setting_moods));
+            arrayDb.add(new item_caidat("Mật khẩu","",R.drawable.icon_setting_password));
+            arrayDb.add(new item_caidat("Ngôn ngữ","",R.drawable.icon_setting_language));
             Adapter=new adapter_caidat(this,R.layout.item_caidat,arrayDb);
             lvDb.setAdapter(Adapter);
             lvDb.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    if(i==0) {
+                        Intent o = new Intent(caidat.this, updateDDKNActivity.class);
+                        //Truyen bien ddkn trong bảng database qua edittext trong file java update
+                        o.putExtra("ddkn", ddkn);
 
-                    Intent o = new Intent(caidat.this, updateDDCKActivity.class);
-                    o.putExtra("loichao",s);
-                    o.putExtra("duongdan", s2);
-                    startActivity(o);
+                        startActivity(o);
+                    }
+                    else if(i==1)
+                    {
+                        Intent o1 = new Intent(caidat.this, updateDDCKActivity.class);
+
+                        o1.putExtra("ddck", ddck);
+                        startActivity(o1);
+                    }
+                    else if(i==2)
+                    {
+
+                    }
 
                 }
             });
--- app/src/main/java/com/example/mattershmily/myapplication/updateDDKNActivity.java
+++ app/src/main/java/com/example/mattershmily/myapplication/updateDDKNActivity.java
@@ -13,45 +13,45 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+public class updateDDKNActivity extends AppCompatActivity {
 
-public class updateDDCKActivity extends AppCompatActivity {
     Button btn_save;
     Toolbar toolbar;
-    TextView txtDoDaichuky;
-    EditText edDoDaichuky;
+    TextView txtDoDaiKinhNguyet;
+    EditText edDoDaiKinhNguyet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_ddck);
+        setContentView(R.layout.activity_update_ddkn);
         addControl();
         // Khởi tạo toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         //Không hiện tiêu đề
         getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setTitle("Độ dài chu kỳ");
+        getSupportActionBar().setTitle("Độ dài kinh nguyệt");
         //Hiện nút back
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent i = getIntent();
-        String title = i.getExtras().getString("ddck","28");
+        String title = i.getExtras().getString("ddkn","3");
 
-        edDoDaichuky.setText(title);
+        edDoDaiKinhNguyet.setText(title);
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                int ddck=Integer.parseInt(edDoDaichuky.getText().toString());
+                int ddkn=Integer.parseInt(edDoDaiKinhNguyet.getText().toString());
                 final String DATABASE_NAME="c.sqlite";
                 SQLiteDatabase Database;
                 ContentValues contentValues=new ContentValues();
-                contentValues.put("dodaichuky",ddck);
+                contentValues.put("dodaikinh",ddkn);
 
-                Database=database.initDatabase(updateDDCKActivity.this,DATABASE_NAME);
+                Database=database.initDatabase(updateDDKNActivity.this,DATABASE_NAME);
                 int id=1;
                 Database.update("caidat",contentValues,"id=?",new String[] {id+""});
-                Toast.makeText(updateDDCKActivity.this,"saved",Toast.LENGTH_LONG).show();
-                Intent i=new Intent(updateDDCKActivity.this,caidat.class);
+                Toast.makeText(updateDDKNActivity.this,"saved",Toast.LENGTH_LONG).show();
+                Intent i=new Intent(updateDDKNActivity.this,caidat.class);
                 startActivity(i);
             }
         });
@@ -59,9 +59,9 @@ public class updateDDCKActivity extends AppCompatActivity {
 
     }
     public void addControl() {
-        txtDoDaichuky = (TextView) findViewById(R.id.txtDoDaichuky);
-        edDoDaichuky= (EditText) findViewById(R.id.edDoDaichuky);
-        btn_save= (Button) findViewById(R.id.save_ddck);
+        txtDoDaiKinhNguyet = (TextView) findViewById(R.id.txtDoDaichuky);
+        edDoDaiKinhNguyet= (EditText) findViewById(R.id.edDoDaiKinhNguyet);
+        btn_save= (Button) findViewById(R.id.btnDoDaiKN);
     }
 
     @Override
@@ -81,5 +81,4 @@ public class updateDDCKActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-    }
-
+}
